9c5c180228052389fd0c2293b9e6d6c5c9391541
Joshua Kapilian <kapgamesandapps2@gmail.com>
2023-06-17 23:34:18 -0400

    First draft with errors

---
+++ spotify.py
@@ -0,0 +1,53 @@
+import json
+import argparse
+from datetime import datetime, timedelta
+
+SPOTIFY_FORMAT = "%Y-%m-%d %H:%M"
+STRUCTURE_FORMAT = "%Y-%m-%d"
+
+def rankWeek(currentWeekRank, args):
+    # print(currentWeekRank)
+    ret = {}
+    for item in currentWeekRank:
+        if currentWeekRank[item] >= args.cutoff:
+            ret[item] = currentWeekRank[item]
+    # TODO: sort
+    return ret
+
+def create_structure(args):
+    currentWeek = None
+    structure = {}
+    currentWeekRank = {}
+    for file in args.input:
+        f = open(file, "r")
+        data = json.loads(f.read())
+        for item in data:
+            date = datetime.strptime(item["endTime"], SPOTIFY_FORMAT)
+            week = date + timedelta(days=-date.weekday())
+            if week != currentWeek:
+                if currentWeek != None:
+                    structure[currentWeek.strftime(STRUCTURE_FORMAT)] = rankWeek(currentWeekRank, args)
+                currentWeek = week
+                currentWeekRank = {}
+            formattedName = f'{item["artistName"]} - {item["trackName"]}'
+            if formattedName in currentWeekRank:
+                print("here")
+                print(currentWeekRank[formattedName])
+                currentWeekRank[formattedName] += item["msPlayed"]
+                print(currentWeekRank[formattedName])
+            else:
+                currentWeekRank[formattedName] = item["msPlayed"]
+        f.close()
+    
+    f = open(args.output, "w")
+    f.write(json.dumps(structure))
+    f.close()
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Parse Spotify data")
+    parser.add_argument('-i', '--input', nargs='+', default=[], help="Input data files")
+    parser.add_argument('-o', '--output', help="Output data file")
+    parser.add_argument('-c', '--cutoff', help="Ms cutoff", type=int)
+    args = parser.parse_args()
+    create_structure(args)
